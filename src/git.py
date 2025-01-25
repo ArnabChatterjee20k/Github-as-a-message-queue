5501a17f827c2ceb5450405feb167ee8e37e7739
arnab <arnabchatterjee.ac.2@gmail.com>
2025-01-26 02:19:10 +0530

    added example consumers and fixed the git upload and workflow action file

--- src/git.py
+++ src/git.py
@@ -5,5 +5,5 @@ class Git:
     
     def push(origin,commit_message="Updates"):
         subprocess.run(["git", "add", "."])
-        # subprocess.run(["git", "commit", "-m", commit_message])
-        # subprocess.run(["git", "push","origin",origin],check=True)
+        subprocess.run(["git", "commit", "-m", commit_message])
+        subprocess.run(["git", "push","origin",origin],check=True)
