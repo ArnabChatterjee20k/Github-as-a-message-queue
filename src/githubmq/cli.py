e066925b8287ad629987dac1b321d710c6de2f6b
arnab <arnabchatterjee.ac.2@gmail.com>
2025-01-26 16:25:46 +0530

    updated the cli

--- src/githubmq/cli.py
+++ src/githubmq/cli.py
@@ -8,9 +8,10 @@ def cli():
 @cli.command()
 @click.option('--repo-url', prompt='GitHub repo URL', help='URL of the GitHub repository')
 @click.option('--auth-token', prompt='GitHub access token', help='GitHub access token')
-def setup(repo_url, auth_token):
+@click.option("--origin",prompt="Origin name, ex-> master,main,etc",help="enter the origin name")
+def setup(repo_url, auth_token,origin="main"):
     """Setup the GitHub repo with consumers and workflows."""
-    github = GithubManager(repo_url=repo_url,auth_token=auth_token)
+    github = GithubManager(repo_url=repo_url,auth_token=auth_token,origin=origin)
     
     # Upload consumers and workflow files
     github.upload_consumer_scripts()
